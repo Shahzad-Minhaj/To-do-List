b971c688b5f1ce1d0ea2f5b45f927be965fa600d
Shahzad-Minhaj <sminhaj.suit@gmail.com>
2023-12-16 13:48:58 +0500

    Second Commit

--- script.js
+++ script.js
@@ -1,27 +1,84 @@
+//======================(original code for to-dolist)==============================
+// let tasks = [];
 
-let x = [];
+// document.getElementById("btn").onclick = function() {
+//   let inputElement = document.getElementById("addTask");
+//   let y = String(inputElement.value);
 
-function addButton() {
+ 
+
+//   tasks.push(y);
+//   inputElement.value = ""; // Clear the input field after pushing the value
+//   updateOutput();
+
+//   document.getElementById("deletebutton").onclick = function(){
+//     document.getElementById("output").innerHTML= tasks
+//   }
+// }
+
+// function updateOutput() {
+//     for(let i=0;i<x.length;i++){
+//   document.getElementById("output").innerHTML = ` ${i+1}: ` + tasks[i]+ '<button id="deletebutton" style="margin-left: 20px;">Delete</button>';
+    
+// }
+// }
+//========================(practice for to do list)=============
+let tasks = [];
+let loop=0;
+
+document.getElementById("btn").onclick= function(){
   let inputElement = document.getElementById("addTask");
   let y = String(inputElement.value);
 
  
 
-  x.push(y);
+  tasks.push(y);
   inputElement.value = ""; // Clear the input field after pushing the value
-  updateOutput();
-
+  
+  document.getElementById("output").innerHTML += `<ul><li>${tasks[loop]} <button id="deletebutton" style="margin-left: 20px;">Delete</button></li></ul>`
+  loop++ 
+  
   document.getElementById("deletebutton").onclick = function(){
-    document.getElementById("output").innerHTML= x
+    document.getElementById("output").innerHTML = ""
   }
+  
 }
 
-function updateOutput() {
-    for(let i=0;i<x.length;i++){
-  document.getElementById("output").innerHTML = ` ${i+1}: ` + x[i]+ '<button id="deletebutton" style="margin-left: 20px;">Delete</button>';
-    
+//===================================(practice)======================
+//===========(Correct piece of code for Adding 20 students to get the result)
+let x=[]
+let i=0;
+document.getElementById("marksBtn").onclick=function(){
+
+  let y = document.getElementById("addStudentMarks")
+  x.push(y.value)
+  y.value= "";
+  
+  
+  if(x[i]<50){
+    document.getElementById("output2").innerHTML += ` Student: ${i+1} got ${x[i]} Marks, Status: <b>Fail</b> Grade: <b>F</b><br>`
+    i++
+  }
+
+  else if(x[i]>=50 && x[i] <60 ){
+    document.getElementById("output2").innerHTML += ` Student: ${i+1} got ${x[i]} Marks, Status: <b>PASS</b> Grade: <b>C-</b><br>`
+    i++
+  }
+
+  else if(x[i]>=60 && x[i] <70 ){
+    document.getElementById("output2").innerHTML += ` Student: ${i+1} got ${x[i]} Marks, Status: <b>PASS</b> Grade: <b>C+</b><br>`
+    i++
+  }
+
+  else if(x[i]>=70 && x[i] <80 ){
+    document.getElementById("output2").innerHTML += ` Student: ${i+1} got ${x[i]} Marks, Status: <b>PASS</b> Grade: <b>B</b><br>`
+    i++
+  }
+  
+  else if(x[i]>=80 ){
+    document.getElementById("output2").innerHTML += ` Student: ${i+1} got ${x[i]} Marks, Status: <b>PASS</b> Grade: <b>A</b><br>`
+    i++
+  }
+
+
 }
-}
-
-
-
